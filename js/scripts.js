2323db3a4a7b58c737029190328885dc8ee94c25
Neville <kilonzoneville@gmail.com>
2018-04-30 17:58:50 +0300

    all changes

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+ $("#log form").submit(function(event) {
+   var emailInput = $("input#subinput").val();
+   $(".email1").text(emailInput);
+   $("#info").show();
+
+   event.preventDefault();
+ });
+});
+
+function openNav() {
+ document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+ document.getElementById("mySidenav").style.width = "0";
+}
+
+$(document).ready(function() {
+ $("#lunch").hover(function() {
+   $('.layer').show();
+ }, function() {
+   $('.layer').hide();
+ });
+});
+
+
+$(document).ready(function() {
+ $("#breakfast").hover(function() {
+   $('.layer1').show();
+ }, function() {
+   $('.layer1').hide();
+ });
+});
